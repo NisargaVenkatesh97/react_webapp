05370cabc8beb6db0e663b11377e93072fe591c5
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2025-07-17 22:15:50 -0500

    Initial commit

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react/jsx-runtime";
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
